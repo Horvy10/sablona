cd199dd2fa23b96c7d042634286cda4c0d27430d
Horvy10 <luky.steam1213@gmail.com>
2025-03-05 00:16:22 +0100

    otazky-nedokoncene

---
+++ otazky.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+// Pole otázok
+$questions = [
+    "Aké sú vaše skúsenosti s PHP?",
+    "Aký je váš obľúbený programovací jazyk?",
+    "Aké sú vaše ciele pre túto stránku?",
+    "Aké je najlepšie zviera?"
+];
+
+// Pole odpovedí
+$answers = [
+    "Mám základné znalosti PHP a skúsenosti, ktoré neviem použiť.",
+    "Môj obľúbený programovací jazyk bude určite PHP.",
+    "Mojím cieľom je prejsť tento predmet.",
+    "Odpoveď bude vždy kačica."
+];
+?>
+ 
+</body>
+</html>
+
